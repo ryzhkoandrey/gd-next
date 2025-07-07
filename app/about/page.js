bcec62e5b975a4e44e877b5df15fb8a79bec5e2d
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-07 22:30:56 +0300

    4

---
+++ app/about/page.js
@@ -0,0 +1,13 @@
+export default function About() {
+   return (
+      <div>
+         <h1>Страница про нас</h1>
+         <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus placeat
+            reprehenderit rerum possimus nisi. Ex, deserunt blanditiis nemo, adipisci
+            voluptatum velit natus saepe ullam, maiores quas quis excepturi.
+            Perspiciatis, nulla?
+         </p>
+      </div>
+   );
+}
--- app/page.js
+++ app/page.js
@@ -1,7 +1,18 @@
-export default function Home() {
+async function fetchData() {
+   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+   const result = await res.json();
+   return result;
+}
+
+export default async function Home() {
+   const posts = await fetchData();
+
    return (
       <div>
          <h1>Главная страница</h1>
+         {posts.map((el) => (
+            <div key={el.id}>{el.title}</div>
+         ))}
       </div>
    );
 }
