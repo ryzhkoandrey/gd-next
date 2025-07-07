f20ee07214311fc91876c1069d78a9cd632e6f2c
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-07 20:00:11 +0300

    3

--- app/client/show/page.js
+++ app/client/show/page.js
@@ -1,3 +1,12 @@
+import Link from 'next/link';
+
 export default function Show() {
-   return <h1>Show Client страница</h1>;
+   return (
+      <div>
+         <h1>Show Client страница</h1>
+         <Link href="/">Главная</Link>
+         <br />
+         <Link href="/client/12345">Клиент 12345</Link>
+      </div>
+   );
 }
