d3fc83c2fe0a0d09b49ba2917e944ba1edf4adc0
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-07-08 13:32:36 +0300

    5

--- app/post/[id]/page.js
+++ app/post/[id]/page.js
@@ -1,5 +1,20 @@
-function Post({ params }) {
-   return <div>{params.id}</div>;
+async function fetchData(id) {
+   const res = await fetch('https://jsonplaceholder.typicode.com/posts/' + id);
+   const result = await res.json();
+   return result;
 }
+
+const Post = async ({ params: { id } }) => {
+   const post = await fetchData(id);
+
+   return (
+      <div>
+         <strong>{post.id}</strong>
+         <h1>{post.title}</h1>
+         <p>{post.body}</p>
+         <p>Avtor id: {post.userId}</p>
+      </div>
+   );
+};
 
 export default Post;
